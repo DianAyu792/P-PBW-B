80644d39f4f81ddf4b2e075ad5fbde429b4ca354
Dian Ayu A <zeanneayu@gmail.com>
2025-10-17 14:58:59 +0700

    Menambahkan README.md Pertemuan4

---
+++ Pertemuan4/LaraPress/resources/views/Kontak.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <style>
+    body {
+      margin: 40px;
+    }
+
+    .container {
+      display: flex;             
+      gap: 40px;                 
+    }
+
+    .column {
+      flex: 1;                  
+      background-color: #cff3ffff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 0 5px rgba(0,0,0,0.1);
+    }
+
+    h1 {
+      color: #000000ff;
+    }
+
+    a {
+      display: inline-block;
+      margin-top: 20px;
+      text-decoration: none;
+      color: #007bff;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<title>Hubungi Kami - LaraPress</title>  
+<body> 
+  <h1>Kontak LaraPress</h1> 
+
+  <div class="container">
+    <div class="column">
+      <h3>Email</h3>
+      <p>larapress123@example.com</p>
+    </div>
+
+    <div class="column">
+      <h3>Nomor Telepon</h3>
+      <p>+62 812-3333-7777</p>
+    </div>
+  </div>
+
+  <a href="/">Kembali ke Halaman Utama</a>
+</body> 
+</html>
